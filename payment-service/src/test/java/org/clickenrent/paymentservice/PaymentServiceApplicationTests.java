c7e34bd632f76fa86ba803542ba1f9c83fe18e47
jsdev777 <shvetsovitaliy@gmail.com>
2025-11-26 23:53:34 +0200

    Initial multi-module project setup

---
+++ payment-service/src/test/java/org/clickenrent/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clickenrent.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rental-service/src/test/java/org/clickenrent/rentalservice/RentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clickenrent.rentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
